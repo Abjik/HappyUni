01aa26f3caa708f58840268cefb5b314d909b735
Abjik <200107009@stu.sdu.edu.kz>
2023-02-25 02:21:32 +0600

    start

--- app/src/main/java/com/example/happyuni/MainActivity.kt
+++ app/src/main/java/com/example/happyuni/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.happyuni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happyuni.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
