d387088342ba2e1bc4eb409859ee10a049e68828
Abjik <200107009@stu.sdu.edu.kz>
2023-02-27 11:07:14 +0600

    сделал пару xml и navbar

--- app/src/main/java/com/example/happyuni/MainActivity.kt
+++ app/src/main/java/com/example/happyuni/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.happyuni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.happyuni.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        navController = Navigation.findNavController(this, R.id.frame_layout)
+//        setupWithNavController(binding.bottomNavigationView, navController)
     }
 
 }
